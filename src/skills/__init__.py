73dc43cae44d3b452898990f8063d44359e87f11
SHIVAM <93383164+ShivamGoyal03@users.noreply.github.com>
2025-02-07 15:46:44 +0530

    minor refactor

--- src/skills/__init__.py
+++ src/skills/__init__.py
@@ -1,7 +1,7 @@
-from flight_skill import FlightSkill
-from hotel_skill import HotelSkill
-from restaurant_skill import RestaurantSkill
-from excursion_skill import ExcursionSkill
+from .flight_skill import FlightSkill
+from .hotel_skill import HotelSkill
+from .restaurant_skill import RestaurantSkill
+from .excursion_skill import ExcursionSkill
 
 __all__ = [
     "FlightSkill",
